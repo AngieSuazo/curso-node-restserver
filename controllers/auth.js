bb5ac0c24dc7825ada650811371d924230b15eef
AngieSuazo <angiefiorella.suazo@urp.edu.pe>
2024-05-21 22:58:48 -0500

    Fin de sección 11 versión 4.0.0

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const {response} = require ('express');
 const bcryptjs= require('bcryptjs');
 const Usuario=require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login= async(req, res= response)=> {
@@ -32,7 +33,7 @@ const login= async(req, res= response)=> {
                 msg: 'Usuario/Password no son correctos -password incorrecto' 
            })
         }
-        //Guardar el JWT
+        //Generar el JWT
         const token = await generarJWT(usuario.id);
     
     
@@ -47,10 +48,51 @@ const login= async(req, res= response)=> {
             msg: 'Contactar al administrador'
         })
     }
+}
 
-
+const googleSignIn=async(req ,res=response)=>{
+    const {id_token}=req.body;
+    try {
+        const {correo,nombre,img}=await googleVerify(id_token);
+        //console.log(googleUser);
+        let usuario =await Usuario.findOne({correo}); //verificar si el correo ya existe en la BD
+        if (!usuario){ //ai usuario no existe. . .
+            //Crear usuario
+            const data ={
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google:true
+            };
+            usuario= new Usuario(data); //creamos
+            await usuario.save();//guardamos en la BD
+        }
+        //Si el usuario en la DB
+        if (!usuario.estado ){ //si el usuario de google tiene su estado false, se niega autenticación en mi aplicación 
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        } 
+        //Generar el JWT
+        const token = await generarJWT(usuario.id);
+ 
+        res.json({
+            msg: 'Todo ok google signin',
+             usuario,
+             token
+        })
+        
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El Token no se pudo verificar'
+        })
+        
+    }
 }
 
 module.exports={
-    login
+    login,
+    googleSignIn
 }
